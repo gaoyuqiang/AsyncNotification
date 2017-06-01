69987ff166cb2c20b8ddbac471f4de69680987be
gaoqingfang <gaoqingfang@58ganji.com>
2017-06-01 22:45:50 +0800

    Initial Commit

---
+++ TestAsyncNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAsyncNotification
+//
+//  Created by gao on 2017/6/1.
+//  Copyright © 2017年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAsyncNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAsyncNotification
+//
+//  Created by gao on 2017/6/1.
+//  Copyright © 2017年 58. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
